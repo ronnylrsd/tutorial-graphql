23c67b5c17822105454222d6145feb91e67fec8c
Ronny Lima Ribeiro da Silva <63603061+ronnylrsd@users.noreply.github.com>
2023-08-09 08:08:05 -0300

    first commit

---
+++ src/main/java/br/com/ronnyribeiro/tutorialgraphql/TutorialGraphqlApplication.java
@@ -0,0 +1,13 @@
+package br.com.ronnyribeiro.tutorialgraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialGraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialGraphqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ronnyribeiro/tutorialgraphql/TutorialGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ronnyribeiro.tutorialgraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
