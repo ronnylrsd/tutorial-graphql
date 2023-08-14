a7c197d2a9e7ee421bc85e576530a7c6b26acccb
Ronny Lima Ribeiro da Silva <63603061+ronnylrsd@users.noreply.github.com>
2023-08-14 15:36:15 -0300

    Feat: connect controller-schema and enable graph

--- src/main/java/br/com/ronnyribeiro/tutorialgraphql/ForumController.java
+++ src/main/java/br/com/ronnyribeiro/tutorialgraphql/ForumController.java
@@ -3,6 +3,9 @@ package br.com.ronnyribeiro.tutorialgraphql;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -13,15 +16,19 @@ public class ForumController {
     @Autowired
     CommentService commentService;
 
-    public Post postById(String id) {
+    //@SchemaMapping(typeName = "Query", value = "postById")
+    @QueryMapping
+    public Post postById(@Argument String id) {
         return postService.postById(id);
     }
 
-    public Collection<Post> createPost(String content) {
+    @MutationMapping
+    public Collection<Post> createPost(@Argument String content) {
         return postService.createPost(content);
     }
 
-    public Collection<Comment> createComment(String content, String postId) {
+    @MutationMapping
+    public Collection<Comment> createComment(@Argument String content, @Argument String postId) {
         return commentService.createComment(content, postId);
     }
 }
